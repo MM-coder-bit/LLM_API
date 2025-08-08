3d68f207eaeb0cf37ebfb9925a4ca6216e937a72
Mateus Marques <marques18@outlook.com>
2025-08-07 23:16:53 -0300

    Testes práticos

--- cliente.py
+++ cliente.py
@@ -8,16 +8,18 @@ import requests
 url = "http://localhost:3000/api"
 
 # Payload (texto de entrada para o LLM)
-payload = {'text': (None, 'Which color is the car in the image?')}
+#payload = {'text': (None, 'Which color is the car in the image?')}
+payload = {'text': (None, 'are there 9 birds in the picture?')}
 #payload = {'text': (None, 'Which color is the elephant in the image?')}
 #payload = {'text': (None, 'What is the dog doing in the image?')}
 #payload = {'text': (None, 'what animal is that in the picture?')}
 
 # Imagem enviada para o modelo
-file = [('image', open('imagens//imagem1.png','rb'))]
+#file = [('image', open('imagens//imagem1.png','rb'))]
 #file = [('image', open('imagens//imagem2.png','rb'))]
 #file = [('image', open('imagens//imagem3.jpg','rb'))]
 #file = [('image', open('imagens//cat.webp','rb'))]
+file = [('image', open('imagens//passaros.jpg','rb'))]
 
 # Cabeçalho
 headers = {'accept': 'application/json'}
